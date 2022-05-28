2cfe2cba6ce117d418142bec1af32ca6ec7ba3ce
Moisés Mariano <moisesduartem@gmail.com>
2022-05-28 15:45:33 -0300

    feat(client): configure login form w/ react hook form

--- web/src/features/auth/login/index.tsx
+++ web/src/features/auth/login/index.tsx
@@ -1,19 +1,29 @@
 import React from 'react';
 import { Button, TextField } from '@mui/material';
+import { useForm } from 'react-hook-form';
 import { BasicCard } from '../../../components/basic-card';
 import './styles.css';
+import { SignInRequest } from './requests';
 
 function Login() {
+  const { handleSubmit, register } = useForm<SignInRequest>();
+
+  const onSubmit = (data: SignInRequest) => {
+    console.log(data);
+  };
+
   return (
     <div className="login">
       <BasicCard className="login__form-card" sx={{ minWidth: 320, maxWidth: 350 }}>
-        <div>
-          <TextField color="primary" label="E-mail" />
-        </div>
-        <div>
-          <TextField color="primary" label="Password" type="password" />
-        </div>
-        <Button variant="contained">Sign In</Button>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div>
+            <TextField {...register('email')} color="primary" label="E-mail" />
+          </div>
+          <div>
+            <TextField {...register('password')} color="primary" label="Password" type="password" />
+          </div>
+          <Button type="submit" variant="contained">Sign In</Button>
+        </form>
       </BasicCard>
     </div>
   );
