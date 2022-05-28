15cdaffd8d7652c677c52e09b824ebb458c16a7d
Moisés Mariano <moisesduartem@gmail.com>
2022-05-28 16:11:10 -0300

    feat(client): validate sign form fields with yup

--- web/src/features/auth/login/index.tsx
+++ web/src/features/auth/login/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import { Button, TextField } from '@mui/material';
 import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 import { BasicCard } from '../../../components/basic-card';
 import './styles.css';
 import { SignInRequest } from './requests';
 
+const schema = yup.object({
+  email: yup.string().label('E-mail').required().email(),
+  password: yup.string().label('Password').required(),
+}).required();
+
 function Login() {
-  const { handleSubmit, register } = useForm<SignInRequest>();
+  const { handleSubmit, formState: { errors }, register } = useForm<SignInRequest>({
+    resolver: yupResolver(schema),
+  });
 
   const onSubmit = (data: SignInRequest) => {
     console.log(data);
@@ -17,10 +26,23 @@ function Login() {
       <BasicCard className="login__form-card" sx={{ minWidth: 320, maxWidth: 350 }}>
         <form onSubmit={handleSubmit(onSubmit)}>
           <div>
-            <TextField {...register('email')} color="primary" label="E-mail" />
+            <TextField
+              {...register('email', { required: true })}
+              error={Boolean(errors.email)}
+              helperText={errors.email?.message}
+              color="primary"
+              label="E-mail"
+            />
           </div>
           <div>
-            <TextField {...register('password')} color="primary" label="Password" type="password" />
+            <TextField
+              {...register('password', { required: true })}
+              error={Boolean(errors.password)}
+              helperText={errors.password?.message}
+              color="primary"
+              label="Password"
+              type="password"
+            />
           </div>
           <Button type="submit" variant="contained">Sign In</Button>
         </form>
