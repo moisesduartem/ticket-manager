31f14797d27d2eb5dfc528fdffefbb8cbb82c1f7
Moisés Mariano <moisesduartem@gmail.com>
2022-06-05 15:15:12 -0300

    fix(api): configure category & tickets seeds

--- api/src/TicketManager.Infra/Migrations/20220605181415_Category_Tickets_Seeds.Designer.cs
+++ api/src/TicketManager.Infra/Migrations/20220605181415_Category_Tickets_Seeds.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TicketManager.Infra.Database;
 
@@ -11,9 +12,10 @@ using TicketManager.Infra.Database;
 namespace TicketManager.Infra.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220605181415_Category_Tickets_Seeds")]
+    partial class Category_Tickets_Seeds
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
