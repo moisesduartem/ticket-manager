fe997ca8fe4be04faf7f8ffbc3307052b5cd8f9c
Moisés Mariano <moisesduartem@gmail.com>
2022-06-06 07:58:56 -0300

    feat(api): ask a bearer token to do any tickets controller's request

--- api/src/TicketManager.Presentation.Api/Controllers/TicketsController.cs
+++ api/src/TicketManager.Presentation.Api/Controllers/TicketsController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using TicketManager.Application.Services;
 
 namespace TicketManager.Presentation.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class TicketsController : BaseApiController
     {
         private readonly TicketsService _ticketsService;
--- api/src/TicketManager.Presentation.Api/Program.cs
+++ api/src/TicketManager.Presentation.Api/Program.cs
@@ -27,6 +27,8 @@ app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
+
 app.UseAuthorization();
 
 app.MapControllers();
